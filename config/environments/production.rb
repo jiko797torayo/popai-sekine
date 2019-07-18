dcb0b3c8a7412c5345d5470be0cb66e4b34593e1
jiko797torayo <oec7dwayne7kj@gmail.com>
2019-07-19 00:12:05 +0900

    ActionCableの設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,7 +45,7 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
-  config.action_cable.allowed_request_origins = [ /http:\/\/.*/, /https:\/\/.*/ ]
+  config.action_cable.allowed_request_origins = [ 'http://www.popaisekine.com/', /http:\/\/www.popaisekine.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
