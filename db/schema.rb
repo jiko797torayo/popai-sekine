490a61bf19ce06f217d160bf77ec9c71c74a6e99
jiko797torayo <oec7dwayne7kj@gmail.com>
2019-07-30 21:22:21 +0900

    レコード既読機能

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_29_142643) do
+ActiveRecord::Schema.define(version: 2019_07_30_114126) do
 
   create_table "admins", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 2019_07_29_142643) do
     t.datetime "deleted_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "trainer_confirmed_at"
   end
 
   create_table "trainer_requests", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
