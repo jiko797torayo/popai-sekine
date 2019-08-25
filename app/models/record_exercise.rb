4a143248657651a4e57e64f46bb5c7c85aa5d886
jiko797torayo <oec7dwayne7kj@gmail.com>
2019-08-25 16:43:03 +0900

    種目内容、セット内容が空白でなければ保存する

--- app/models/record_exercise.rb
+++ app/models/record_exercise.rb
@@ -1,8 +1,11 @@
 class RecordExercise < ApplicationRecord
   acts_as_paranoid
+
   belongs_to :record
   belongs_to :exercise
   belongs_to :part
   has_many :exercise_details, dependent: :destroy
   accepts_nested_attributes_for :exercise_details, reject_if: :all_blank, allow_destroy: true
+
+  validates :exercise_details, presence: true
 end
