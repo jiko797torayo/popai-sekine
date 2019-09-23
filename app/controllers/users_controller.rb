57cc870ac675fae88316c0212865c4b08a38ee72
jiko797torayo <oec7dwayne7kj@gmail.com>
2019-09-24 07:33:39 +0900

    UsersControllerのcomment_confirmアクションのリファクタリング

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,18 +2,25 @@ class UsersController < ApplicationController
   layout 'users'
   before_action :authenticate_user!
 
-  def comment_confirm
-    before_path = begin
-                    Rails.application.routes.recognize_path(request.referer)
-                  rescue StandardError
-                    return false
-                  end
-    return false unless before_path[:controller].in?(['users/records', 'users/client_records'])
-    return false unless before_path[:action].eql?('show')
+  private
 
-    partner_comments = Comment.where(record_id: before_path[:id]).where.not(user_id: current_user.id)
-    partner_comments.each do |comment|
-      comment.update(receiver_confirmed_at: Time.zone.now) if comment.receiver_confirmed_at.nil?
-    end
+  def comment_confirm
+    path = referer_path
+    return unless records_show_path?(path)
+
+    unread_comments = Comment.where.not(user_id: current_user.id).
+                      where(record_id: path[:id], receiver_confirmed_at: nil)
+    unread_comments.update_all(receiver_confirmed_at: Time.zone.now)
+  end
+
+  def referer_path
+    Rails.application.routes.recognize_path(request.referer)
+  rescue StandardError
+    false
+  end
+
+  def records_show_path?(path)
+    path[:controller].in?(['users/records', 'users/client_records']) &&
+      path[:action].eql?('show')
   end
 end
